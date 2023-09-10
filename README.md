a1214dda0ab747c90ff0d0471c7979b33d755092
Rado Dodek <radoslav@dodek.sk>
2023-09-10 12:34:00 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# ai_helper
+# AI Helper
+
+Simple AI helper utility for daily tasks.
+
+This utility is specifically designed to be used with global hotkeys, allowing you to quickly and easily find the answer 
+you need. By using your clipboard, you can speed up your workflow. Simply copy the text or concept you want to rephrase 
+or explain, and then press the global hotkey. The application will take the content from the clipboard as the input 
+and then copy the answer back to the clipboard.
+
+When you're using the application, simply press `Ctrl+Enter` in question field to get the answer.
+If you want to exit the application, just press `ESC`.
+
+## Pre-requisites
+
+- Python 3.6 or higher
+- OpenAI API key. Set the key in the `OPENAI_API_KEY` environment variable.
+
+## Install dependencies
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## Setup global hotkeys in Ubuntu
+
+This application is designed to work with global hotkeys. To set it up in Ubuntu, simply follow these steps:
+
+Open _Settings -> Keyboard Shortcuts -> Custom Shortcuts_ and create the following shortcuts:
+
+### Rewrite text
+
+This feature automatically rewrites any text currently in the clipboard to improve clarity and grammar.
+The newly rewritten text will be displayed in the user interface and then placed back into the clipboard.
+
+| Name               | Suggested shortcut   | Command                        | Description                   |
+|--------------------|----------------------|--------------------------------|-------------------------------|
+| AI Helper: Rewrite | `Ctrl+Alt+Shift+F10` | `python3 ai_helper.py Rewrite` | Rewrite text using AI helper. |
+
+### Ask question
+
+This feature takes the content of the clipboard and puts it into the question field in the following format:
+
+```
+Explain: <CLIPBOARD_CONTENT>
+```
+
+To send this question to OpenAI, simply press `Ctrl+Enter`. The answer will be displayed in the user interface.
+
+| Name           | Suggested shortcut   | Command                    | Description               |
+|----------------|----------------------|----------------------------|---------------------------|
+| AI Helper: Ask | `Ctrl+Alt+Shift+F11` | `python3 ai_helper.py Ask` | Ask AI helper a question. |
+
---
+++ ai_helper.py
@@ -0,0 +1,159 @@
+import errno
+import sys
+import threading
+from pathlib import Path
+from tkinter import PhotoImage
+
+import customtkinter
+import openai
+import pyperclip
+from customtkinter import CTkFont
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+
+def app_help():
+    print("Usage:")
+    print(" ", sys.argv[0], "<ACTION>")
+    print(" Supported actions: Rewrite, Ask")
+
+
+class App(customtkinter.CTk):
+    MAX_SIZE = 1000
+
+    def __init__(self):
+        super().__init__(className="AI Helper")
+
+        self.SUPPORTED_ACTIONS = {
+            "Rewrite": self.ui_execute_rewrite,
+            "Ask": self.execute_ask_question
+        }
+
+        if len(sys.argv) < 2:
+            print("Missing parameter ACTION")
+            app_help()
+            sys.exit(errno.EPERM)
+
+        self.action = sys.argv[1]
+        if self.SUPPORTED_ACTIONS.get(self.action) is None:
+            print('Unsupported action:', self.action)
+            app_help()
+            sys.exit(errno.EPERM)
+
+        # UI
+        monospace_font = CTkFont(family="monospace", size=16, weight="normal")
+
+        # configure window
+        self.title("AI Helper")
+        self.geometry(f"{1000}x{700}")
+        root_dir = Path(__file__).resolve().parent
+        self.iconphoto(False, PhotoImage(file=root_dir / "assets/app-icon.png"))
+
+        # configure grid layout
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+
+        self.textbox_question = customtkinter.CTkTextbox(self, font=monospace_font, height=150)
+        self.textbox_question.grid(row=0, column=0, columnspan=2, sticky="nsew")
+
+        # Question button
+        self.answer_button = customtkinter.CTkButton(master=self, text=self.action, command=self.answer_button_event)
+        self.answer_button.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+
+        # Label
+        self.info_label = customtkinter.CTkLabel(self, text="", font=customtkinter.CTkFont(size=14, weight="bold"))
+        self.info_label.grid(row=1, column=1, padx=20, pady=(5, 5))
+
+        # Answer textbox
+        self.textbox_answer = customtkinter.CTkTextbox(self, font=monospace_font)
+        self.textbox_answer.grid(row=2, column=0, columnspan=2, sticky="nsew")
+
+        # Initialize
+        self.question = pyperclip.paste()
+        if self.action == 'Ask':
+            self.question = 'Explain: ' + self.question
+        self.textbox_question.insert("0.0", self.question)
+        self.textbox_question.focus_set()
+
+        # Bind keyboard shortcuts
+        self.textbox_question.bind(
+            '<Control_L><Return>',
+            # Return value "break" means that the event should not be processed by the default handler
+            lambda event: "break" if self.answer_button_event() is None else None
+        )
+        self.textbox_question.bind('<Escape>', lambda event: self.quit())
+        self.textbox_answer.bind('<Escape>', lambda event: self.quit())
+
+        # Initial execution at the startup
+        if self.action == 'Rewrite' and self.question is not None:
+            self.answer_button_event()
+
+    def answer_button_event(self):
+        self.set_working_state('Working...')
+        self.execute_in_thread(lambda: self.SUPPORTED_ACTIONS[self.action](
+            self.clip_text(str(self.textbox_question.get("0.0", "end")), self.MAX_SIZE)), ())
+
+    def set_working_state(self, message):
+        self.answer_button.configure(state='disabled')
+        self.info_label.configure(text=message)
+
+    def unset_working_state(self, message):
+        self.answer_button.configure(state='normal')
+        self.info_label.configure(text=message)
+
+    def quit(self):
+        self.destroy()
+
+    def ui_execute_rewrite(self, text_to_rewrite):
+        # Execute the prompt
+        prompt = f"Please rewrite the following text for more clarity and make it grammatically correct. Give me the " \
+                 f"updated text. The updated text should be correct grammatically and stylistically and should be " \
+                 f"easy to follow and understand. Don't make it too formal. Include only improved text no other " \
+                 f"commentary.\n\nThe text to check:\n---\n{text_to_rewrite}\n---\n\nImproved text: "
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", temperature=0.9,
+            messages=[{"role": "user", "content": prompt}]
+        )
+        result = completion.choices[0].message.content
+
+        self.textbox_answer.delete("0.0", "end")
+        self.textbox_answer.insert("0.0", result)
+
+        pyperclip.copy(result)
+        self.unset_working_state('Copied to clipboard')
+
+    def execute_ask_question(self, question):
+        # Execute the prompt
+        completion = openai.ChatCompletion.create(
+            model="gpt-4", temperature=0,
+            messages=[{"role": "user", "content": question}]
+        )
+        result = completion.choices[0].message.content
+
+        self.textbox_answer.delete("0.0", "end")
+        self.textbox_answer.insert("0.0", result)
+
+        self.info_label.configure(text='')
+        self.unset_working_state('')
+
+    @staticmethod
+    def clip_text(text, max_size):
+        if text is None:
+            return None
+
+        if len(text) > max_size:
+            return text[:max_size].strip()
+        else:
+            return text.strip()
+
+    @staticmethod
+    def execute_in_thread(callback, args):
+        thread = threading.Thread(target=callback, args=args)
+        thread.start()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
