032247a6f6e57437714346a9f43abe7f48aef5a4
Rado Dodek <radoslav@dodek.sk>
2023-09-10 20:00:10 +0200

    Code cleanup

--- ai_helper.py
+++ ai_helper.py
@@ -11,6 +11,12 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 
+def app_help():
+    print("Usage:")
+    print(" ", sys.argv[0], "<ACTION>")
+    print(" Supported actions: Rewrite, Ask")
+
+
 class App(customtkinter.CTk):
     MAX_SIZE = 1000
 
@@ -24,13 +30,13 @@ class App(customtkinter.CTk):
 
         if len(sys.argv) < 2:
             print("Missing parameter ACTION")
-            self.help()
+            app_help()
             sys.exit(errno.EPERM)
 
         self.action = sys.argv[1]
         if self.SUPPORTED_ACTIONS.get(self.action) is None:
             print('Unsupported action:', self.action)
-            self.help()
+            app_help()
             sys.exit(errno.EPERM)
 
         # UI
@@ -76,23 +82,16 @@ class App(customtkinter.CTk):
             self.answer_button_event()
             # self.SUPPORTED_ACTIONS[self.action](self.clip_text(str(self.question), self.MAX_SIZE))
 
-    def help(self):
-        print("Usage:")
-        print(" ", sys.argv[0], "<ACTION>")
-        print(" Supported actions: Rewrite, Ask")
-
     def answer_button_event(self):
         self.set_working_state('Working...')
         self.execute_in_thread(lambda: self.SUPPORTED_ACTIONS[self.action](
             self.clip_text(str(self.textbox_question.get("0.0", "end")), self.MAX_SIZE)), ())
 
     def set_working_state(self, message):
-        self.textbox_question.configure(state='disabled')
         self.answer_button.configure(state='disabled')
         self.info_label.configure(text=message)
 
     def unset_working_state(self, message):
-        self.textbox_question.configure(state='normal')
         self.answer_button.configure(state='normal')
         self.info_label.configure(text=message)
 
@@ -130,7 +129,8 @@ class App(customtkinter.CTk):
         self.info_label.configure(text='')
         self.unset_working_state('')
 
-    def clip_text(self, text, max_size):
+    @staticmethod
+    def clip_text(text, max_size):
         if text is None:
             return None
 
@@ -139,47 +139,12 @@ class App(customtkinter.CTk):
         else:
             return text.strip()
 
-    def execute_in_thread(self, callback, args):
+    @staticmethod
+    def execute_in_thread(callback, args):
         thread = threading.Thread(target=callback, args=args)
         thread.start()
 
-    def delayed_execution(self, callback, delay_in_sec):
-        timer = threading.Timer(delay_in_sec, callback)
-        timer.start()
-
-    def notify_copied_to_clipboard(self):
-        self.info_label.configure(text='Copied into the clipboard')
-        self.delayed_execution(lambda: self.info_label.configure(text=''), 2)
-
-
-# Check the parameter
-# Possible values:
-# - "rewrite"
-# - "ask"
 
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
-#
-#
-# # Event Loop to process "events" and get the "values" of the inputs
-# while True:
-#     event, values = window.read()
-#     if (
-#             event == sg.WIN_CLOSED or
-#             event == 'Cancel' or
-#             event == '-ORIGINAL_TEXT-ESCAPE' or
-#             event == '-GENERATED_TEXT-ESCAPE'
-#     ):
-#         break
-#
-#     if event == 'Copy':
-#         pyperclip.copy(values['-GENERATED_TEXT-'])
-#         window['-INFO_LABEL-'].update('Copied into the clipboard')
-#
-#     else:
-#         original_text = values['-ORIGINAL_TEXT-']
-#         SUPPORTED_ACTIONS[action](original_text)
-#
-# window.close()
