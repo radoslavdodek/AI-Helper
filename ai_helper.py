c13c8852e74f83a186ebc931f6ca4500b6efcc11
Rado Dodek <radoslav@dodek.sk>
2024-04-14 22:10:07 +0200

    Change the model to 'gpt-4-turbo'

--- ai_helper.py
+++ ai_helper.py
@@ -56,7 +56,7 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 # See https://openai.com/blog/new-embedding-models-and-api-updates
-default_model = 'gpt-4-turbo-preview'
+default_model = 'gpt-4-turbo'
 
 
 def app_help():
