fb17d919c6caa5226e3c1fa5871f9cf49adb6020
Rado Dodek <radoslav@dodek.sk>
2024-12-15 01:49:06 +0100

    Test

--- ai_helper.py
+++ ai_helper.py
@@ -112,7 +112,7 @@ class App(customtkinter.CTk):
         if self.action == 'CustomPrompt':
             question_button_title = 'Execute custom prompt'
 
-        self.geometry(f"{1000}x{700}")
+        self.geometry(f"{800}x{1000}")
         self.iconphoto(False, PhotoImage(file=self.app_path / "assets/app-icon.png"))
 
         # configure grid layout
@@ -135,7 +135,7 @@ class App(customtkinter.CTk):
                                                                 state=customtkinter.DISABLED,
                                                                 text='',
                                                                 image=PhotoImage(file=self.app_path / "assets/copy-icon.png"),
-                                                                width=20,
+                                                                width=30,
                                                                 bg_color="transparent",
                                                                 fg_color="transparent",
                                                                 hover_color="#F0F0F0",
@@ -259,7 +259,7 @@ class App(customtkinter.CTk):
             prompt = self.render_custom_prompt(custom_prompt, pyperclip.paste())
             print(prompt)
             completion = client.chat.completions.create(
-                model=default_model, temperature=0,
+                model=default_model, temperature=0.5,
                 messages=[{"role": "user", "content": prompt}]
             )
             result = completion.choices[0].message.content
